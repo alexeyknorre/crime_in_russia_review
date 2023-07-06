15c6a7258841783022d2be561d847035cdc440e4
Alexey Knorre <a.v.knorre@gmail.com>
2023-07-06 18:04:36 -0400

    small changes

--- produce_figures.R
+++ produce_figures.R
@@ -16,7 +16,7 @@ figure2 <- ggplot(data = dat, aes(x=year,y=share_prosecuted)) +
   geom_point()+
   geom_line() +
   expand_limits(y = 0)+
-  scale_x_continuous(breaks = seq(2012,2020,1),limits = c(2012,2020),
+  scale_x_continuous(breaks = seq(2012,2021,1),limits = c(2012,2021),
                      name = "")+
   scale_y_continuous(name = 'Share of prosecutions with administrative prejudice',
                      labels = scales::label_percent(accuracy = 1L)) +
@@ -26,7 +26,7 @@ figure2 <- ggplot(data = dat, aes(x=year,y=share_prosecuted)) +
         panel.grid.minor = element_blank(),
         panel.border = element_blank(),
         panel.background = element_blank(),
-        text=element_text(size=12, family="Times New Roman"))
+        text=element_text(size=10, family="Times New Roman"))
 
 ggsave("figures/fig2_prejudice.pdf",figure2,
        width = 6, height = 4, bg = "white", dpi = 300)
@@ -62,7 +62,7 @@ p <- ggplot(data = cr, aes(x = Year, y = Measure.Values,
         panel.grid.minor = element_blank(),
         panel.border = element_blank(),
         panel.background = element_blank(),
-        text=element_text(size=12, family="Times New Roman"),
+        text=element_text(size=10, family="Times New Roman"),
         strip.background =element_blank())+
   expand_limits(y=0)
 
@@ -88,7 +88,7 @@ figure4 <- ggplot(data = dat, aes(x=year,y=prison_population_rate)) +
         panel.grid.minor = element_blank(),
         panel.border = element_blank(),
         panel.background = element_blank(),
-        text=element_text(size=12, family="Times New Roman")) 
+        text=element_text(size=10, family="Times New Roman")) 
 
 ggsave("figures/fig4_prison_population_rate.pdf",
        figure4,width = 6, height = 4, bg = "white", dpi = 300)
@@ -112,7 +112,7 @@ figure5 <- ggplot(data = dat, aes(x=year,y=share_early_released_adult_prisoners/
         panel.grid.minor = element_blank(),
         panel.border = element_blank(),
         panel.background = element_blank(),
-        text=element_text(size=12, family="Times New Roman")) 
+        text=element_text(size=10, family="Times New Roman")) 
 
 ggsave("figures/fig5_early_released.pdf",
        figure5,width = 6, height = 4, bg = "white", dpi = 300)
